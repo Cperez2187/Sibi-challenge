7b7700adf723e386662b15e8e94bc373fac70e64
cperez2187 <cperez2187@gmail.com>
2017-12-29 12:21:37 -0700

    Intial setup

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// Include the Main React Dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Main from './components/Main.js';
+
+const App = () => <Main />;
+
+// Renders the contents accordingly
+ReactDOM.render(<App />, document.getElementById("app"));
+
